87d6785825274821969a328c1f0a0f06f25fef52
nxavierb <32310173+nxavierb@users.noreply.github.com>
2021-01-16 15:27:07 +0300

    2

--- connect.py
+++ connect.py
@@ -55,28 +55,9 @@ def create_tables():
     #                             EMAIL VARCHAR(80) NOT NULL,
     #                             PASSWORD VARCHAR(200) NOT NULL
     #                             )"""
-    conn = None
-    try:
-        # read the connection parameters
-        params = config()
-        # connect to the PostgreSQL server
-        conn = psycopg2.connect(**params)
-        cur = conn.cursor()
-        # create table one by one
-        for command in commands:
-            cur.execute(command)
-        # close communication with the PostgreSQL database server
-        cur.close()
-        # commit the changes
-        conn.commit()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
+    connect()
 
 
 
 if __name__ == '__main__':
-    connect()
     create_tables()
