f1ff1be25e3aecfe35048c4932b5001bf4c564aa
nxavierb <32310173+nxavierb@users.noreply.github.com>
2021-01-16 23:04:25 +0300

    2

--- database_app.py
+++ database_app.py
@@ -1,14 +1,14 @@
 import psycopg2 as dbapi2
 from user import User
 class store():
-    def add_user(conf, users):
+    def add_user(self,conf, users):
         with dbapi2.connect(conf) as connection:
             cursor = connection.cursor()
             query2 = """INSERT INTO USERS (NAME, SURNAME, USERNAME, EMAIL, PASSWORD ) VALUES (%s, %s, %s, %s, %s)"""
             cursor.execute(query2, (users.name, users.surname, users.username, users.email, users.password))
             connection.commit()
 
-    def get_user(conf, username):
+    def get_user(self,conf, username):
         with dbapi2.connect(conf) as connection:
             cursor = connection.cursor()
             query = "SELECT NM, SURNAME, NICKNAME, EMAIL, PASSWORD FROM USERS WHERE NICKNAME = %s"
@@ -17,14 +17,14 @@ class store():
                 (name, surname, nickname, email, password) = row
                 return User(name, surname, nickname, email, password)
                 
-    def update_password(conf, username, new_password):
+    def update_password(self,conf, username, new_password):
         with dbapi2.connect(conf) as connection:
             cursor = connection.cursor()
             query = "UPDATE USERS SET PASSWORD = %s WHERE (NICKNAME = %s)"
             cursor.execute(query, (new_password, username))
             connection.commit()    
-            
-    def is_exist(conf, username):
+
+    def is_exist(self,conf, username):
         with dbapi2.connect(conf) as connection:
             cursor = connection.cursor()
             query = "SELECT PASSWORD FROM USERS WHERE NICKNAME = %s"
--- server.py
+++ server.py
@@ -12,7 +12,9 @@ import json
 import os
 import psycopg2 as dbapi2
 import re
+
 app = Flask(__name__)
+conn = psycopg2.connect(database="recipe", user = "postgres", password = "1573596248", host = "127.0.0.1", port = "5432")
 
 
 def get_elephantsql_dsn(vcap_services):
@@ -26,38 +28,6 @@ def get_elephantsql_dsn(vcap_services):
     return dsn
 
 lm=LoginManager()
-
-
-def connect():
-    """ Connect to the PostgreSQL database server """
-    conn = None
-    try:
-        # read connection parameters
-        params = config()
-
-        # connect to the PostgreSQL server
-        print('Connecting to the PostgreSQL database...')
-        conn = psycopg2.connect(**params)
-		
-        # create a cursor
-        cur = conn.cursor()
-        
-	# execute a statement
-        print('PostgreSQL database version:')
-        cur.execute('SELECT version()')
-
-        # display the PostgreSQL database server version
-        db_version = cur.fetchone()
-        print(db_version)
-       
-	# close the communication with the PostgreSQL
-        cur.close()
-    except (Exception, psycopg2.DatabaseError) as error:
-        print(error)
-    finally:
-        if conn is not None:
-            conn.close()
-            print('Database connection closed.')
 
 @app.route("/")
 def home_page():
@@ -99,9 +69,8 @@ def register_page():
         return render_template('register.html')
     elif request.method == 'POST':
         user = User(request.form['name'], request.form['surname'], request.form['username'], request.form['password'], request.form['email'])
-        print(user.name)
-        if store.is_exist(app.config['dsn'], request.form['username']):
-            return render_template('register.html', error = "This username is already taken.")
+        # if store.is_exist(app.config['dsn'], request.form['username']):
+        #     return render_template('register.html', error = "This username is already taken.")
         store.add_user(app.config['dsn'], user)
         return redirect(url_for('login_page'))
 
