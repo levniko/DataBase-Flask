f72e0c83d8f871b9dd2a18277556d6103166ea64
nxavierb <32310173+nxavierb@users.noreply.github.com>
2021-01-16 14:40:43 +0300

    1

--- server.py
+++ server.py
@@ -106,5 +106,7 @@ def initialize_database():
         db_cursor.execute(query)
 
 if __name__ =="__main__":
+
+    connect()
     port = app.config.get("PORT", 5000)
     app.run(host="127.0.0.1", port=port)
